859550613b31e7924faa81037ba12b063745686c
Bethany Ingraham <BethFIngraham@gmail.com>
2024-08-30 01:14:44 -0400

    Testing Middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,55 @@
+const Users = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(
+    `A ${req.method} request was made to ${req.url}, at ${new Date().toLocaleString()}
+  `);
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const user = await Users.getById(id);
+    if(!user) {
+      res.status(404).json({
+        message: 'user not found'
+      })
+    } else {
+      req.user = user;
+      next();
+    }
+  }catch (err) {
+    res.status(500).json({
+      message: 'error getting user'
+    })
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body;
+  if(
+    name !== undefined 
+    && typeof name === 'string' 
+    && name.length 
+    && name.trim().length
+  ) {
+    req.name = name;
+    next();
+  } else {
+    res.status(400).json({
+      message: 'missing required name field'
+    })
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId, 
+  validateUser,
+};
